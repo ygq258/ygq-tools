1126fac011b38d8825d2c30e491d2456359fa386
ygq <ygq258@yeah.net>
2019-11-06 10:28:20 +0800

    1.测试占位符号

--- src/main/java/org/ygq/tools/test/jtd/Test2.java
+++ src/main/java/org/ygq/tools/test/jtd/Test2.java
@@ -8,6 +8,10 @@ public class Test2 {
 	public static void main(String[] args) {
 		
 		System.out.println(String.format("购买【%s】的数量与联系人的数量不符", "门票"));
+		System.out.println(String.format("购买【%s】的数量与联系人的数量不符", "a"));
+		System.out.println(String.format("购买【%s】的数量与联系人的数量不符", "A"));
+		System.out.println(String.format("购买【%S】的数量与联系人的数量不符", "a"));
+		System.out.println(String.format("购买【%S】的数量与联系人的数量不符", "A"));
 		
 		Date date = new Date();
 		
